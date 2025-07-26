b5301a4d1a0243cfbfa5d89fa5ab0d1592984f50
Matheus616 <matheussc616@gmail.com>
2025-07-26 18:39:30 -0300

    atualizacoes css

--- scrips.js
+++ scrips.js
@@ -3,7 +3,7 @@ let chave ="d768a72943db91b64b1860fb1fe90442"
 function colocaNaTela(dados){
     console.log(dados)
 
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".cidade").innerHTML = "Tempo " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp)  + "°C"
     document.querySelector(".Névoa").innerHTML = dados.weather[0].description
     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon +".png"
